4d9ad948edeb4048e2101777a5db92ea92db2070
Slavo Borodin <74551212+SBorodin347@users.noreply.github.com>
2020-11-25 19:12:38 +0100

    Create VypisPolaVopacnomPoradi.c

---
+++ VypisPolaVopacnomPoradi.c
@@ -0,0 +1,25 @@
+/*Vypis prvkov pola v opacnom poradi*/
+
+
+int main(){
+    //pocet prvkov v poli
+    int n;
+    printf("zadaj n: ");
+    scanf("%d", &n);
+    int pole[n];
+    //zadavame prvky pola
+    int i;
+    for (i = 0; i<n; i++){
+        printf("zadaj element masiva: ");
+        scanf("%d", &pole[i]);
+    }
+    //vypis prvkov pola v opacnom poradi
+    //n-1 = posledny element pola, preto zaciname od n-1 
+    //akonahle i sa stane 0 (indexom prveho elementa) cyklus sa ukonci
+    for (i = n-1; i>=0; i--){
+        printf("%d ", pole[i]);
+    }
+    putchar('\n');
+    
+    return 0;
+}

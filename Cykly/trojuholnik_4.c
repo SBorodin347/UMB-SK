e7d7c9383e81d4ded59d353f05ca3a9fb1e14aa1
Slavo Borodin <74551212+SBorodin347@users.noreply.github.com>
2020-12-08 00:48:34 +0100

    Create trojuholnik_4.c

---
+++ Cykly/trojuholnik_4.c
@@ -0,0 +1,26 @@
+ /*
+ 
+ i
+ 1 *****
+ 2 ****
+ 3 ***
+ 4 **
+ 5 *
+ 
+ */
+
+
+
+
+int main(){
+    int riadok, i, j;
+    printf ("zadaj cislo riadkov: ");
+    scanf("%d", &riadok);
+    for (i = 1; i<=riadok; i++){ //pocet riadkov
+        for(j = 0; j<riadok-i+1; j++){ //pocet hviezd
+            putchar('*');
+        }
+        putchar('\n');
+    }
+    return 0;
+}
